278933ae4fd9df116c04061f8f09c1d7a28ef3bb
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-10-03 12:55:58 -0600

    Update EventHandler.cs
    Added Late Calls

--- Scripts/Guymon/Core/EventHandler.cs
+++ Scripts/Guymon/Core/EventHandler.cs
@@ -8,6 +8,15 @@ using UnityEngine.Events;
 namespace Guymon.DesignPatterns {
     public static class EventHandler
     {
+        private class EventActionCall
+        {
+            public string eventID;
+            public UnityAction<EventArgs> action;
+            public EventActionCall(string eventID, UnityAction<EventArgs> action) {
+                this.eventID = eventID;
+                this.action = action;
+            }
+        }
         /// <summary>
         /// Subscribe a Method to an Event ID
         /// </summary>
@@ -20,6 +29,10 @@ namespace Guymon.DesignPatterns {
                 allEvents[eventID].Add(action);
             }
         }
+        private static List<EventActionCall> lateAddCalls = new List<EventActionCall>();
+        public static void AddListenerLate(string eventID, UnityAction<EventArgs> action) {
+            lateAddCalls.Add(new EventActionCall(eventID, action));
+        }
         /// <summary>
         /// Unsubscribe a Method to an Event ID
         /// </summary>
@@ -30,6 +43,10 @@ namespace Guymon.DesignPatterns {
                 allEvents[eventID].Remove(action);
             }
         }
+        private static List<EventActionCall> lateRemoveCalls = new List<EventActionCall>();
+        public static void RemoveListenerLate(string eventID, UnityAction<EventArgs> action) {
+            lateRemoveCalls.Add(new EventActionCall(eventID, action));
+        }
         /// <summary>
         /// Invokes All Methods Attached to the Event ID
         /// </summary>
@@ -39,6 +56,18 @@ namespace Guymon.DesignPatterns {
             foreach(UnityAction<EventArgs> unityAction in allEvents[eventID]) {
                 unityAction?.Invoke(args);
             }
+
+            foreach (EventActionCall call in lateAddCalls)
+            {
+                AddListener(call.eventID, call.action);
+            }
+            foreach (EventActionCall call in lateRemoveCalls)
+            {
+                RemoveListener(call.eventID, call.action);
+            }
+
+            lateAddCalls.Clear();
+            lateRemoveCalls.Clear();
         }
 
         private static Dictionary<string, List<UnityAction<EventArgs>>> allEvents = new Dictionary<string, List<UnityAction<EventArgs>>>();
